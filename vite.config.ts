990468f5f128604af439926b08ce64b79f19eaa6
Manos Markogiannakis <manosmarkogiannakis@Manoss-Air.home>
2026-06-02 17:04:35 +0300

    vite.config update

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,16 @@
 import { defineConfig } from "vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import { TanStackStartVite } from "@tanstack/react-start/plugin";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
-    TanStackStartVite(),
+    tanstackStart(), // 👈 Handles Tanstack Start flawlessly 
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     react(),
     tailwindcss(),
     tsconfigPaths(),
   ],
 });
-// trigger build
